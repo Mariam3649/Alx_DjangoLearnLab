6fc7c763a8a46728a94115d69b924891f11f146a
Mariam3649 <mariamfarid820@gmail.com>
2025-09-28 00:59:30 +0300

    Add IsAuthenticated permission to BookViewSet

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,7 +6,10 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]   
 
