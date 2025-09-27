0441309a20fa443d7835c7f56400d1f875623ba9
Mariam3649 <mariamfarid820@gmail.com>
2025-09-28 00:54:59 +0300

    Add authentication and permissions with DRF token

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
      'books',
 
 
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
