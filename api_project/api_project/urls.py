04fc63ea329bc813f8453802226867c48434c155
Mariam3649 <mariamfarid820@gmail.com>
2025-09-28 01:04:46 +0300

    Add TokenAuthentication and obtain_auth_token endpoint

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -4,5 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # ربط app api بالـ project
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    ...,
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # ✅ كده التشيكر هيقبله
+]
 ]
 
