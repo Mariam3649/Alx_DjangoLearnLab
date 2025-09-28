909e807970d4e8f904ac3452e45d4806f4d9902c
Mariam3649 <mariamfarid820@gmail.com>
2025-09-28 19:36:45 +0300

    Fix filter_backends to include filters.OrderingFilter and SearchFilter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,27 +1,20 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters import rest_framework as filters
-
-from .models import Book
-from .serializers import BookSerializer
-
+from rest_framework import generics, filters as drf_filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    # Add filter, search, ordering backends
-    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [
+        filters.DjangoFilterBackend,
+        drf_filters.SearchFilter,
+        drf_filters.OrderingFilter,
+    ]
 
-    # Fields for filtering
     filterset_fields = ['title', 'author__name', 'publication_year']
-
-    # Fields for searching
     search_fields = ['title', 'author__name']
-
-    # Fields for ordering
     ordering_fields = ['title', 'publication_year']
-    ordering = ['title']  # default ordering
+    ordering = ['title']
 
